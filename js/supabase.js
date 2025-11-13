48c6bb7336073b4397894eeec1b1eb1f62e7fe98
Lexionas74 <cla0055@brightonsc.vic.edu.au>
2025-11-13 23:38:57 +1100

    1

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,12 @@
 // /js/supabase.js
-// DO NOT put keys, URLs, or anything else here.
 
-const supabase = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
+// YOUR PROJECT URL
+const SUPABASE_URL = "https://ygnfuztbwsngjihrinlr.supabase.co";
+
+// YOUR NEW PUBLISHABLE API KEY
+const SUPABASE_ANON_KEY = "sb_publishable_2wnacEtDB7Q7uvrjfEDMKw_1Q1niq6q";
+
+// Create client
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+console.log("Supabase client initialized");
