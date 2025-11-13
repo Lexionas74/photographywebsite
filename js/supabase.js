bfa41d41676ca9146c7e501a1553cbf731dba07c
Lexionas74 <cla0055@brightonsc.vic.edu.au>
2025-11-14 10:12:11 +1100

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,15 +1,15 @@
 // /js/supabase.js
-console.log("Loading supabase.js...");
 
 const SUPABASE_URL = "https://ygnfuztbwsngjihrinlr.supabase.co";
 
-// REAL ANON KEY:
 const SUPABASE_ANON_KEY =
 "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlnbmZ1enRid3NuZ2ppaHJpbmxyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5ODUwOTMsImV4cCI6MjA3ODU2MTA5M30.YlKPGDE3hfF9mOIVbUEQQFtN8gwt6L0MOYldH1AOMGw";
+
+console.log("Initializing Supabase client…");
 
 window.supabase = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
 );
 
-console.log("Supabase client initialized");
+console.log("Supabase client initialized.");
