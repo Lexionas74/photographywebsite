82b8d44a09095609eb2a04630327a49b087b6c0c
Lexionas74 <cla0055@brightonsc.vic.edu.au>
2025-11-14 09:59:06 +1100

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -4,8 +4,7 @@ console.log("Loading supabase.js…");
 
 // Your project info
 const SUPABASE_URL = "https://ygnfuztbwsngjihrinlr.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_2wnacEtDB7QrurjfEDMKw_1QIni9q";
-
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlnbmZ1enRid3NuZ2ppaHJpbmxyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5ODUwOTMsImV4cCI6MjA3ODU2MTA5M30.YlKPGDE3hfF9mOIVbUEQQFtN8gwt6L0MOYldH1AOMGw";
 // Create client using the CDN global
 const client = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
