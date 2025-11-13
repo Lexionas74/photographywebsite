4edc67e4f491671584c7fc5710ebd3915b26a62c
Lexionas74 <cla0055@brightonsc.vic.edu.au>
2025-11-13 16:11:03 +1100

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,7 @@
 // js/supabase.js
 // Vanilla JS loader for Supabase "images" table
 
-const SUPABASE_URL = "https://ygnfuztbwsngjihrinlr.supabase.co";
+const SUPABASE_URL = "https://ygnfuztbwsngjihrinlr.functions.supabase.co/upload";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlnbmZ1enRid3NuZ2ppaHJpbmxyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5ODUwOTMsImV4cCI6MjA3ODU2MTA5M30.YlKPGDE3hfF9mOIVbUEQQFtN8gwt6L0MOYldH1AOMGw"
 
 async function fetchGalleryImages(gallery) {
